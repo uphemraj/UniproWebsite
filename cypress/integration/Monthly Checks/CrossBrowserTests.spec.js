f1406f1a5998aa2944385e9b1407db033faf7bc8
Hemraj Bedassee <hemraj.bedassee@unipro.io>
2022-03-23 10:05:07 +0400

    added browserstack

--- cypress/integration/Monthly Checks/CrossBrowserTests.spec.js
+++ cypress/integration/Monthly Checks/CrossBrowserTests.spec.js
@@ -5,5 +5,4 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 //https://www.unipro.io/
 it('load Home page',() => {
     cy.visit('https://www.unipro.io/', {timeout: 40000});
-cy.xpath('/html/body/div[3]/div/div/div/div[3]/div[2]/button').should('be.visible').click()
 })
